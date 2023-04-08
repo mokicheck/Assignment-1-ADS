0815fbfe1baff058c7aed93934291e4c51eebea3
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-04-09 02:29:25 +0600

    In this code I add  recursive "isPrime" function, that uses a recursive approach to check if the given number is prime or composite.

---
+++ Example 3/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    static int i = 2;
+
+    // Main function that reads user input and calls isPrime function
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==1 || isPrime(n))
+            System.out.println("Prime");
+        else
+            System.out.println("Composite");
+    }
+
+    // Function to check if a number is prime or composite
+    public static boolean isPrime(int n) {
+        // If i reaches n/2 + 1 without finding a divisor, n is prime
+        if(i == n/2 + 1)
+            return true;
+        // If n is divisible by i, n is composite
+        if(n % i == 0)
+            return false;
+        // Increase i and call isPrime recursively
+        i++;
+        return isPrime(n);
+    }
+}
