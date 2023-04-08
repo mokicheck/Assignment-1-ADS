f9edc522d521243f968bd7221e3ef56118899f2a
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-04-09 03:01:09 +0600

    This code reads an integer input from the user and calculates the factorial of the input using a recursive function called factorial(). The result is then printed to the console.

---
+++ Example 4 lab1/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = factorial(n);
+        System.out.println(result);
+    }
+
+    // Recursive function to calculate factorial
+    public static int factorial(int N) {
+        // Base case: if N is 0 or 1, return 1
+        if (N <= 1) {
+            return 1;
+        }
+        // Recursive case: multiply N by the factorial of N-1
+        return factorial(N-1) * N;
+    }
+}
