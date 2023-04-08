1bffd9c83c89b40ff96687694d3eb311418fcfbf
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-04-08 10:43:00 +0600

    There is implementation of example1 from lab1, where fucn finds min of arr

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Create an array of integers
-        int[] ints = {5, 10, 1, 32, 3, 45};
+        int[] arr = {5, 10, 1, 32, 3, 45};
 
         // Initialize a variable for the minimum value
-        int min = Integer.MAX_VALUE;
-
-        // Iterate over all the elements in the array
-        for (int n : ints){
-            // If the current value is less than the current minimum value, update the minimum value
-            if(n < min){
-                min = n;
-            }
+        int min = findMin(arr, arr.length);
+        System.out.println("Min of arr is: " + min + " " + arr.length);
+    }
+/* findMin func takes an integer array arr and an integer n as input,
+and returns the minimum value in the first n elements of the array.
+ */
+    public static int findMin(int[] arr, int n){
+        if (n==1)
+                return arr [0];
+        else{
+            //checking the minimum of the first n-1 elements of the array (recursively)
+            //The base case is when n is 1, in which case the function returns the first element of the array.
+            int min = findMin(arr, n-1);
+            return Math.min(min, arr[n-1]);
         }
 
-        // Print the minimum value found in the array
-        System.out.println("Your min value is: " + min);
     }
 }
